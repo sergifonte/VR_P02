d713e763ea17df9ce5e4c586e373b1c062ee904a
emmacasvil <emma.casassas@estudiantat.upc.edu>
2026-04-13 12:46:45 +0200

    array de materials del semàfor

---
+++ Assets/Scripts/TrafficLights/ColorChange.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ColorChange : MonoBehaviour
+{
+    public Material trafficLights;
+    public Material[] colors = { }; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
