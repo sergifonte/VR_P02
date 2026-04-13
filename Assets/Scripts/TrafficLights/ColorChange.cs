851aef5faef10e19d1b31f5cd806d2e5e5d4af89
marinuugh <marina.rodriguez.guasch@estudiantat.upc.edu>
2026-04-13 18:27:59 +0200

    prova codi semafor

--- Assets/Scripts/TrafficLights/ColorChange.cs
+++ Assets/Scripts/TrafficLights/ColorChange.cs
@@ -6,15 +6,67 @@ using UnityEngine;
 public class ColorChange : MonoBehaviour
 {
     public Material trafficLights;
-    public Material[] colors = { }; 
+    //public Material[] colors = { }; 
+    public Material red;
+    public Material green;
+
+    private Renderer rend;
+
+    private bool isRed;
+
+    public enum ExpectedAction { None, Brake, Accelerate }
+    public ExpectedAction expectedAction;
+
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        StartCoroutine(LightCycle());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LightCycle()
     {
-        
+        while (true)
+        {
+            isRed = !isRed;
+
+            if (isRed)
+            {
+                rend.material = red;
+                expectedAction = ExpectedAction.Brake;
+                Debug.Log("RED --> Press Brake");
+            }
+            else
+            {
+                rend.material = green;
+                expectedAction = ExpectedAction.Accelerate;
+                Debug.Log("GREEN --> Press Accelerator");
+            }
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    public void OnBrakePressed()
+    {
+        if (expectedAction == ExpectedAction.Brake)
+        {
+            Debug.Log("Correct: Brake!");
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+        }
+    }
+
+    public void OnAcceleratePressed()
+    {
+        if (expectedAction == ExpectedAction.Accelerate)
+        {
+            Debug.Log("Correct: Accelerate!");
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+        }
     }
 }
